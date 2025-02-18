fd40bf69ed228cb95818b6e18a6f0a18251d9c90
牛朗 <jinpeng.njp@alibaba-inc.com>
2025-02-18 22:35:54 +0800

    delete windows hide window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -319,21 +319,6 @@ pub fn run() {
             #[cfg(target_os = "macos")]
             app.set_activation_policy(tauri::ActivationPolicy::Accessory);
 
-            #[cfg(target_os = "windows")]
-            {
-                use tauri::WindowBuilder;
-                // 创建一个隐藏的主窗口
-                WindowBuilder::new(
-                    app,
-                    "main", /* 这是窗口的唯一标识符 */
-                    tauri::WindowUrl::default(),
-                )
-                .title("Bing Wallpaper")
-                .visible(false)
-                .skip_taskbar(true)
-                .build()?;
-            }
-
             let tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .menu(&Menu::with_items(app, &[
